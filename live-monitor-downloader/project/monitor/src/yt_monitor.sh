e6dbb11d9bc7dcc6f849615d89de676e7f1003aa
Louis Law, louiscklaw <louiscklaw@users.noreply.github.com>
2024-05-02 18:48:53 +0800

    'update,'

--- live-monitor-downloader/project/monitor/src/yt_monitor.sh
+++ live-monitor-downloader/project/monitor/src/yt_monitor.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Set the maximum interval duration in seconds
-MAX_INTERVAL_SEC=16
+MAX_INTERVAL_SEC=60
 
 # Set the minimum interval duration in seconds
-MIN_INTERVAL_SEC=15
+MIN_INTERVAL_SEC=45
 
 # Define the command to run
 # openbox-poe-seat\src\carousell-click-burner\start_chrome.js
