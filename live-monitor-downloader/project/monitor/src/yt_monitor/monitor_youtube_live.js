e6a49adf898263f5a3faf71e2c9a406a8bcbb2fe
Louis Law, louiscklaw <louiscklaw@users.noreply.github.com>
2024-05-02 19:30:04 +0800

    'update,'

--- live-monitor-downloader/project/monitor/src/yt_monitor/monitor_youtube_live.js
+++ live-monitor-downloader/project/monitor/src/yt_monitor/monitor_youtube_live.js
@@ -22,7 +22,7 @@ console.log('start chrome js');
       for (let j = 0; j < 3; j++) {
         try {
           await page.goto(url);
-          await page.waitForTimeout(5 * 1000);
+          await page.waitForTimeout(3 * 1000);
           break;
         } catch (error) {
           console.log('error during getting to the page');
